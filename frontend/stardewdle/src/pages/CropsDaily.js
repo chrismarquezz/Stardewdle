5da9adcd8408f27d6c6d805f0c6a587cab19bef1
chrismarquezz <chrismarquez00@icloud.com>
2025-05-19 23:20:02 -0400

    Implement initial popups and routing

---
+++ frontend/stardewdle/src/pages/CropsDaily.js
@@ -0,0 +1,7 @@
+export default function CropsDaily() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-yellow-100">
+      <h1 className="text-5xl font-bold text-yellow-800">Crops - Daily 🌾</h1>
+    </div>
+  );
+}
---
+++ frontend/stardewdle/src/pages/FishRandom.js
@@ -0,0 +1,7 @@
+export default function FishRandom() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-5xl font-bold text-blue-800">Fish - Random 🐟</h1>
+    </div>
+  );
+}
