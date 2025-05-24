8c0ba3cf02d4f882a34bc55891af1639159025e1
osid54 <osid.potato@gmail.com>
2025-05-23 22:53:56 -0400

    Moved title

--- frontend/stardewdle/src/pages/Game.js
+++ frontend/stardewdle/src/pages/Game.js
@@ -5,28 +5,32 @@ export default function Game() {
   const navigate = useNavigate();
   return (
     <div
-      className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex justify-center items-center flex-col"
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col items-center"
       style={{ backgroundImage: "url('/images/background.jpg')" }}
     >
-      <div
-        onClick={() => {
-          new Audio("/sounds/mouseClick.mp3").play();
-          navigate("/");
-        }}
-        className="relative w-[624px] cursor-pointer group transform transition-transform duration-200 hover:scale-105"
-      >
-        <img
-          src="/images/stardewdleLogo.png"
-          alt="Stardewdle Home"
-          className="w-full transition-opacity duration-200 group-hover:opacity-0"
-        />
-        <img
-          src="/images/stardewdleLogo.png"
-          alt="Stardewdle Home Hover"
-          className="absolute top-0 left-0 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"
-        />
+      <div className="w-full flex justify-center pt-2">
+        <div
+          onClick={() => {
+            new Audio("/sounds/mouseClick.mp3").play();
+            navigate("/");
+          }}
+          className="relative w-[624px] cursor-pointer group transform transition-transform duration-200 hover:scale-105"
+        >
+          <img
+            src="/images/stardewdleLogo.png"
+            alt="Stardewdle Home"
+            className="w-full transition-opacity duration-200 group-hover:opacity-0"
+          />
+          <img
+            src="/images/stardewdleLogo.png"
+            alt="Stardewdle Home Hover"
+            className="absolute top-0 left-0 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+          />
+        </div>
       </div>
+
       <GameBox />
+      
     </div>
   );
 }
