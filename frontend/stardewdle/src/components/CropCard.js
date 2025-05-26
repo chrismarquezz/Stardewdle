d3956fb3548e7a92a40501a1781463d9ba50c26c
osid54 <osid.potato@gmail.com>
2025-05-25 22:14:10 -0400

    Generelized and editted the buttons

--- frontend/stardewdle/src/components/CropCard.js
+++ frontend/stardewdle/src/components/CropCard.js
@@ -1,4 +1,4 @@
-export default function CropCard({ crop, isSelected, onClick, isMuted}) {
+export default function CropCard({ crop, isSelected, onClick, isMuted }) {
   //const { isMuted } = useSound();
 
   const formattedName = crop.name
@@ -8,14 +8,14 @@ export default function CropCard({ crop, isSelected, onClick, isMuted}) {
   return (
     <div
       onClick={() => {
-        if (!isMuted){
+        if (!isMuted) {
           new Audio("/sounds/select.mp3").play();
         }
-          
-onClick(crop)}}
-      className={`relative w-16 h-16 cursor-pointer p-1 flex items-center justify-center group ${
-        isSelected ? "border-4 border-green-400" : "border-0 border-transparent"
-      }`}
+
+        onClick(crop)
+      }}
+      className={`relative w-16 h-16 clickable p-1 flex items-center justify-center group ${isSelected ? "border-4 border-green-400" : "border-0 border-transparent"
+        }`}
       style={{
         backgroundImage: "url('/images/tile-bg.png')",
         backgroundSize: "cover",
@@ -31,16 +31,16 @@ onClick(crop)}}
 
       {/* Custom Label Tooltip */}
       <div
-  className="absolute -top-5 left-1/2 -translate-x-1/2 px-3 py-1 flex items-center justify-center text-xl font-medium text-[#BC6131] text-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-10 whitespace-nowrap"
-  style={{
-    backgroundImage: "url('/images/label.png')",
-    backgroundSize: "100% 100%",
-    backgroundRepeat: "no-repeat",
-    height: "28px",
-  }}
->
-  {formattedName}
-</div>
+        className="absolute -top-5 left-1/2 -translate-x-1/2 px-3 py-1 flex items-center justify-center text-xl font-medium text-[#BC6131] text-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-10 whitespace-nowrap"
+        style={{
+          backgroundImage: "url('/images/label.png')",
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "no-repeat",
+          height: "28px",
+        }}
+      >
+        {formattedName}
+      </div>
 
     </div>
   );
