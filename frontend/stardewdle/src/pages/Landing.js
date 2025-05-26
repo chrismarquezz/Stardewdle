0114dc6d8d89a2a4f74a36ae420253baeda8273f
chrismarquezz <chrismarquez00@icloud.com>
2025-05-25 20:12:08 -0400

    Added credits and GitHub buttons

--- frontend/stardewdle/src/pages/Landing.js
+++ frontend/stardewdle/src/pages/Landing.js
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { useSound } from "../context/SoundContext";
+import { useState } from "react";
 
 export default function Landing() {
   const { isMuted } = useSound();
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div
@@ -64,9 +66,68 @@ export default function Landing() {
       className="absolute top-0 left-0 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"
     />
   </div>
+  <div className="flex gap-2 mt-2">
+  <button
+  onClick={() => {
+      if (!isMuted) {
+        new Audio("/sounds/mouseClick.mp3").play();
+      }
+      window.open('https://github.com/chrismarquezz/Stardewdle', '_blank')}}
+  className="relative w-14 h-14"
+>
+  <img
+    src="/images/github.png"
+    alt="GitHub"
+    className="absolute inset-0 w-full h-full object-contain hover:opacity-0 transition-opacity duration-200"
+  />
+  <img
+    src="/images/github-hover.png"
+    alt="GitHub Hover"
+    className="w-full h-full object-contain"
+  />
+</button>
+
+
+  <button
+  onClick={() => {
+      if (!isMuted) {
+        new Audio("/sounds/mouseClick.mp3").play();
+      }
+      setShowModal(true);}}
+  className="relative w-14 h-14"
+>
+  <img
+    src="/images/credits.png"
+    alt="Info"
+    className="absolute inset-0 w-full h-full object-contain hover:opacity-0 transition-opacity duration-200"
+  />
+  <img
+    src="/images/credits-hover.png"
+    alt="Info Hover"
+    className="w-full h-full object-contain"
+  />
+</button>
+
+
 </div>
 
-  
+</div>
+
+  {showModal && (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
+    <div className="bg-white rounded-lg p-6 w-96 shadow-lg relative">
+      <button
+        onClick={() => setShowModal(false)}
+        className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+      >
+        &times;
+      </button>
+      <h2 className="text-lg font-semibold mb-4">Modal Title</h2>
+      <p>This is your modal content.</p>
+    </div>
+  </div>
+)}
+
 </div>
 
   );
