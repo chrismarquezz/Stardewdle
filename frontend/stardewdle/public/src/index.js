15edeef88192754a3a2f94a7c59eb7713f3fb45e
osid54 <osid.potato@gmail.com>
2025-06-26 21:22:05 -0400

    Patch 3.10

---
+++ frontend/stardewdle/public/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { SoundProvider } from "./context/SoundContext";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <SoundProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </SoundProvider>
+);
+
+reportWebVitals();
