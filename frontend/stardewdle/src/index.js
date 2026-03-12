4dce2e01e18530570e8875a39cb786a34b9d9e73
chrismarquezz <chrismarquez00@icloud.com>
2026-03-11 21:45:25 -0400

    Added Vercel analytics

--- frontend/stardewdle/src/index.js
+++ frontend/stardewdle/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 import { SoundProvider } from "./context/SoundContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <SoundProvider>
     <React.StrictMode>
       <App />
+      <Analytics />
     </React.StrictMode>
-  </SoundProvider>
+  </SoundProvider>,
 );
